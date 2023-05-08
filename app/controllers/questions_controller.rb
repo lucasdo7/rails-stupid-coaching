3d6f4bcf9c8199edf7f196e38a4475136fa79083
Lucas Mendes <lucasbiocelp@gmail.com>
2023-05-08 15:35:21 -0300

    organizei o stupid coach
    Signed-off-by: Lucas Mendes <lucasbiocelp@gmail.com>

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,23 +1,21 @@
 class QuestionsController < ApplicationController
 
-  def home
+def home
+end
+
+def ask_coach
+end
+
+def answer_coach
+  @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question == 'i am going to work'
+      @answer = 'Great!'
+    elsif @question.ends_with?('?')
+      @answer = 'Silly question, get dressed and go to work!.'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
-
-  def ask_coach
-  end
-
-
-  def answer_coach
-    @question = params[:question]
-      if @question.blank?
-        @answer = "I can't hear you!"
-      elsif @question == 'i am going to work'
-        @answer = 'Great!'
-      elsif @question.ends_with?('?')
-        @answer = 'Silly question, get dressed and go to work!.'
-      else
-        @answer = "I don't care, get dressed and go to work!"
-      end
-  end
-
 end
