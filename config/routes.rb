83011739029c5fca8e503d37632fa68122671321
Lucas Mendes <lucasbiocelp@gmail.com>
2023-05-08 15:28:58 -0300

    arrumei o homepage
    Signed-off-by: Lucas Mendes <lucasbiocelp@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # root "articles#index"
   # get 'path', to: 'controller#action'
-  get 'ask', to: 'questions#ask_coach'
   root to: 'questions#home'
+  get 'ask', to: 'questions#ask_coach'
   get 'answer', to: 'questions#answer_coach'
 end
